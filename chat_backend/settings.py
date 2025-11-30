5c638710d45daced007cd56e3f9879c259771965
affilpm <affilpm2004@gmail.com>
2025-11-30 16:56:30 +0530

    setup: Install and configure core API infrastructure (DRF & JWT)
    Initializes the project with Django REST Framework for API creation and implements stateless authentication using JWT. This is the foundation for all future API endpoints.

--- chat_backend/settings.py
+++ chat_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'accounts',
 ]
 
@@ -128,3 +131,18 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    "ALGORITHM": "HS256",                       
+    "SIGNING_KEY": SECRET_KEY,                      
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
